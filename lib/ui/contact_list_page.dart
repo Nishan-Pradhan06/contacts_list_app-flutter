c6f781d150c9bcc6b232a6e6de25e8d314d7997d
nishan <nishanpradhan500@gmail.com>
2023-08-19 21:40:20 +0545

    adding_vertical_list_view

--- lib/ui/contact_list_page.dart
+++ lib/ui/contact_list_page.dart
@@ -10,6 +10,111 @@ class ContactListPage extends StatelessWidget {
         centerTitle: true,
         title: const Text("Contacts"),
       ),
+      //children are nothing than list widget this is ths syntax for creaing list
+      //to make the the children column heugh match we use mainaxisSize
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "Contact test",
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
